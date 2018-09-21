f6e7ef17a3e68665cc237827e5736ec0f2e2a43e
Zac Colley <hi@zaccolley.com>
2018-09-21 12:03:49 +0100

    Don't use server anymore

---
+++ service-worker.js
@@ -0,0 +1,96 @@
+const version = "0.0.1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(version + "fundamentals").then(cache => {
+      return cache.addAll(["/style.css", "/client.js", "/index.html"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      var networked = fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve);
+
+      return cached || networked;
+
+      function fetchedFromNetwork(response) {
+        var cacheCopy = response.clone();
+
+        caches.open(version + "pages").then(function add(cache) {
+          cache.put(event.request, cacheCopy);
+        });
+
+        return response;
+      }
+
+      function unableToResolve() {
+        return new Response("<h1>Service Unavailable</h1>", {
+          status: 503,
+          statusText: "Service Unavailable",
+          headers: new Headers({
+            "Content-Type": "text/html"
+          })
+        });
+      }
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => {
+            return !key.startsWith(version);
+          })
+          .map(key => {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+});
+
+function sendMessageToClient(event, obj) {
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => {
+      if (event.notification.data.url !== client.url) {
+        return;
+      }
+
+      client.postMessage(obj);
+    });
+  });
+}
+
+self.addEventListener("notificationclose", event => {
+  event.waitUntil(
+    (async function() {
+      sendMessageToClient(event, { close: true });
+    })()
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  console.log("event", event);
+
+  event.waitUntil(
+    (async function() {
+      sendMessageToClient(event, {
+        dataSentToNotification: event.notification.data,
+        action: event.action
+      });
+    })()
+  );
+});
