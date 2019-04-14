6ac73bd0e08580ada606742a09494bbe5b5c5013
Zac Colley <hi@zaccolley.com>
2019-04-14 17:03:55 +0100

    ok what are these commits

--- index.js
+++ index.js
@@ -499,12 +499,9 @@ class App extends Component {
       return;
     }
 
-    console.log('hi service worker')
-
     navigator.serviceWorker.register('service-worker.js');
 
     navigator.serviceWorker.addEventListener("message", event => {
-      console.log('message', event)
       const { data } = event;
 
       const dontDoAnthing = data.close || data.dataSentToNotification.test; // test notification
@@ -617,8 +614,9 @@ class App extends Component {
       return;
     }
     
+    this.setTimerCurrentTime(0);
+    this.setTimerPaused(false);
     this.setTimerRunning(false);
-    this.setTimerPaused(true);
   }
 
   handleTimerRepeatAtEndEnabledClick() {
